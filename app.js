a3b7165be49a7bfc65a3cae1785e4ff6509f146f
AmoghaK1 <amogha.khare@gmail.com>
2025-01-26 19:34:33 +0530

    new file:   app.js 	new file:   controllers/userController.js 	new file:   models/userModel.js 	new file:   routes/userRoutes.js 	new file:   views/layouts/footer.ejs 	new file:   views/layouts/header.ejs 	new file:   views/users/registration.ejs

---
+++ app.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/NDA_db");
+const express = require("express");
+const app = express();
+
+//for user route
+const userRoute = require('./routes/userRoutes');
+app.use('/', userRoute);
+
+
+app.listen(3000, ()=>{
+    console.log("Server started on Port 3000...");
+   
+})
+
---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const user_route = express();
+
+user_route.set('view engine', 'ejs');
+user_route.set('viewes', './views/users');
+const userController = require('../controllers/userController');
+user_route.get('/register', userController.loadRegister);
+
+module.exports = user_route;
