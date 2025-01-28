a77798d3ac37fdfa6c01966d2a7234039cdb2190
Hetavi Modi <hetavimodi29@gmail.com>
2025-01-28 12:39:00 +0530

    modified:   app.js 	renamed:    ALL FOLDERS/student-dashboard/bank.png -> public/images/bank.png 	renamed:    ALL FOLDERS/student-dashboard/clipboard.png -> public/images/clipboard.png 	renamed:    ALL FOLDERS/student-dashboard/image 13.png -> public/images/image 13.png 	renamed:    ALL FOLDERS/student-dashboard/image 14.png -> public/images/image 14.png 	renamed:    ALL FOLDERS/student-dashboard/image 5.png -> public/images/image 5.png 	renamed:    ALL FOLDERS/student-dashboard/pfp_final_1.png -> public/images/pfp_final_1.png 	renamed:    ALL FOLDERS/student-dashboard/schedule.png -> public/images/schedule.png 	new file:   public/stylesheets/comingsoon.css 	new file:   public/stylesheets/login.css 	renamed:    public/stylesheets/registration.css -> public/stylesheets/signup.css 	new file:   public/stylesheets/stdashboard.css 	new file:   views/comingsoon.ejs 	new file:   views/login.ejs 	renamed:    views/users/registration.ejs -> views/signup.ejs 	new file:   views/student-dashboard.ejs

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/NDA_db");
 const express = require("express");
 const app = express();
+port = 7000
+
+app.set("view engine","ejs");
 app.use(express.static('./public'));
 //for user route
-const userRoute = require('./routes/userRoutes');
-app.use('/', userRoute);
+// const userRoute = require('./routes/userRoutes');
+// app.use('/', userRoute);
 
 
-app.listen(3000, ()=>{
-    console.log("Server started on Port 3000...");
-   
+app.get('/',(req,res)=>{
+    res.render('comingsoon')
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login',{name:"Hetavi"})
+})
+
+app.get('/signup',(req,res)=>{
+    res.render('signup')
+})
+
+app.get('/st-dashboard',(req,res)=>{
+    res.render('student-dashboard')
+})
+
+app.listen(port, ()=>{
+    console.log(`Server started on Port ${port}...`);
 })
 
