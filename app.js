2325d3a409f31e071895e74ed1af2faa046692cc
AmoghaK1 <amogha.khare@gmail.com>
2025-02-18 21:29:14 +0530

    modified:   .env 	modified:   app.js

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ require("./config/passport")(passport);
 const app = express();
 port = 7000;
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    
   }).then(() => {
     console.log("Connected to MongoDB!");
   }).catch(err => {
